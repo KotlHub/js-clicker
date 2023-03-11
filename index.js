d2c07c2c682941439947c8b001eeefed1e297bfc
RusHi PeaceYes <89914892+KotlHub@users.noreply.github.com>
2023-03-11 07:26:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+let TIMEOUT = 5000;
+let LEVEL = 1; //уровень
+let clicks = 0; //количество кликов за раунд
+let totalClicks = localStorage.getItem('ttl'); //общее количество кликов
+let isShop = false;
+const GAMEOVER = "Game Over";
+
+// main buttons
+const shop = document.querySelector('#shop');
+const level = document.querySelector('#level');
+const display = document.querySelector('#display');
+const button = document.querySelector('#button');
+const newButton = document.querySelector('#newButton');
+const counter = document.querySelector('#counter');
+const total = document.querySelector('#total');
+
+const mainDiv = document.querySelector('#main');
+
+// shop buttons
+
+const shopDiv = document.querySelector('#shopDiv');
+shopDiv.style.display = "none";
+
+
+// const mainElements = [];
+// mainElements.push(level, display, button, counter, total);
+
+button.onclick = start;
+shop.onclick = showShop;
+
+function formatTime(ms) {
+  return Number.parseFloat(ms / 1000).toFixed(2);
+}
+
+function start() {
+  if (isShop)
+  {
+    return;
+  }
+  const startTime = Date.now();
+
+  level.textContent = "Level " + LEVEL;
+
+  display.textContent = formatTime(TIMEOUT);
+  button.onclick = () => {
+  counter.textContent = clicks++;
+  total.textContent = "TOTAL " + totalClicks++;
+  localStorage.setItem('ttl', totalClicks);
+  }
+
+  const interval = setInterval(() => {
+    const delta = Date.now() - startTime;
+    display.textContent = formatTime(TIMEOUT - delta);
+  }, 100);
+
+  const timeout = setTimeout(() => {
+    newButton.onclick = start;
+    button.onclick = start;
+    display.textContent = GAMEOVER;
+
+    clearInterval(interval);
+    clearTimeout(timeout);
+  }, TIMEOUT);
+  if(LEVEL++ && LEVEL >= 5)
+  {
+    newButton.style.right = getRandomInt(80) + "%";
+    newButton.style.top = getRandomInt(80) + "%";
+  }
+
+  clicks = 0;
+  //LEVEL++;
+}
+
+function showShop() {
+  if(!isShop)
+  {
+  shop.textContent = "Back";
+  mainDiv.style.display = "none";
+  shopDiv.style.display = "flex";
+  isShop = !isShop;
+  }
+
+  else if(isShop)
+  {
+  shop.textContent = "Shop";
+  mainDiv.style.display = "flex";
+  shopDiv.style.display = "none";
+  isShop = !isShop;
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
